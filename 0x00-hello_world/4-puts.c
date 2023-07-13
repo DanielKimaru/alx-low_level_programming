54a71b2499a84fea3f09ad00bacac040f9a38721
DanielKimaru <danielkimaru904@gmail.com>
2023-07-13 16:47:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point 
+ * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (succes)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts(("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+	}
